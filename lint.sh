d58a9a5799ea8eee992035125558a59eba44e246
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-10-02 23:36:01 -0400

    format: tweak lint/format scripts

--- lint.sh
+++ lint.sh
@@ -1,2 +1,2 @@
 #/usr/bin/env sh
-clang-tidy src/*.[ch]pp -config='' -- -std=c++17 -I./build/_deps/sfml-src/include
+clang-tidy src/*.[ch]pp -config='' -- -std=c++20 -I./build/_deps/sfml-src/include -I./third-party/ $@
