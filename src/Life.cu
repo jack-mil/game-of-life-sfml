9f4442225e9bd231f47479e36afc1b4888adf806
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-11-09 13:57:40 -0500

    remove old Life methods and organize

--- src/Life.cu
+++ src/Life.cu
@@ -47,9 +47,6 @@ Life::Life(size_t height, size_t width, Mode mode, uint threads)
     assert(mode == Mode::Normal);
 
     // Allocte device memory (host already allocated as a vector<State>)
-    // checkCudaErrors(cudaMalloc(&d_bfr_current, sizeof(State) * m_width * m_height));
-    // checkCudaErrors(cudaMalloc(&d_bfr_next, sizeof(State) * m_height * m_width));
-
     checkCudaErrors(cudaMallocPitch(&d_bfr_current, &d_current_pitch,
                                     sizeof(State) * m_width, m_height));
     checkCudaErrors(cudaMallocPitch(&d_bfr_next, &d_next_pitch,
@@ -68,7 +65,7 @@ Life::Life(size_t height, size_t width, Mode mode, uint threads)
         ((uint)m_width + m_threads->x - 1) / m_threads->x,
         ((uint)m_height + m_threads->y - 1) / m_threads->y,
     }; // AKA Grid size
-    
+
     printf("Life %dx%d\n", m_width, m_height);
     printf("CUDA : blockDim=(%u,%u,%u), gridDim=(%u,%u,%u)\n",
            m_threads->x, m_threads->y, m_threads->z,
@@ -106,6 +103,31 @@ void Life::seedRandom()
 }
 
 /**
+ * Convert row,col specifier to the 1D vector access
+ * Reads from current state
+ */
+inline Life::State Life::getCell(size_t row, size_t col) const
+{
+    return m_bfr_current[row * m_width + col];
+}
+
+/**
+ * Return a collection of all the row, col points that are currently alive.
+ * Compiler will use RVO and move semantics so this avoids unnecessary copying (probably)
+ */
+std::vector<std::pair<int, int>> Life::getLiveCells() const
+{
+    std::vector<std::pair<int, int>> liveCells;
+    for (size_t row = 0; row < m_height; ++row) {
+        for (size_t col = 0; col < m_width; ++col) {
+            if (this->getCell(row, col) == State::Alive) {
+                liveCells.emplace_back(row, col);
+            }
+        }
+    }
+    return liveCells;
+}
+/**
  * Run one iteration of the Game of Life
  * Use different parallelization techniques according to the current mode
  */
@@ -126,24 +148,7 @@ void Life::doOneGeneration()
 
     // swap the std::vectors. This only swaps the underlying pointers,
     // not the contained data. Very cheap and fast (hopefully)
-    // std::swap(m_bfr_current, m_bfr_next);
-}
-
-/**
- * Return a collection of all the row, col points that are currently alive.
- * Compiler will use RVO and move semantics so this avoids unnecessary copying (probably)
- */
-std::vector<std::pair<int, int>> Life::getLiveCells() const
-{
-    std::vector<std::pair<int, int>> liveCells;
-    for (size_t row = 0; row < m_height; ++row) {
-        for (size_t col = 0; col < m_width; ++col) {
-            if (this->getCell(row, col) == State::Alive) {
-                liveCells.emplace_back(row, col);
-            }
-        }
-    }
-    return liveCells;
+    std::swap(m_bfr_current, m_bfr_next);
 }
 
 __global__ void deviceOneGeneration(uint8_t* now, uint8_t* next,
@@ -216,9 +221,6 @@ void Life::updateCudaNormal()
                                  sizeof(State) * m_width, m_height,
                                  cudaMemcpyDeviceToHost));
     // new gen stored in bfr_next
-    // swap the std::vectors. This only swaps the underlying pointers,
-    // not the contained data. Very cheap and fast (hopefully)
-    std::swap(m_bfr_current, m_bfr_next);
 }
 void Life::updateCudaManaged()
 {
@@ -226,124 +228,3 @@ void Life::updateCudaManaged()
 void Life::updateCudaPinned()
 {
 }
-
-/**
- * Run the game of life rules for the specified rows
- * @param start_row first row to process
- * @param end_row last row to process
- */
-// inline void Life::process_chunk(size_t start_row, size_t end_row)
-// {
-//     for (size_t row = start_row; row < end_row; ++row) {
-//         for (size_t col = 0; col < this->m_width; ++col) {
-//             const auto& state = this->simulateSingleCell(row, col);
-//             this->setCell(row, col, state);
-//         }
-//     }
-// }
-
-/**
- * Applies the Game of Life rules on a single cell, and return the next state
- *
- * @param current Read only access the the current state (for counting neighbors)
- * @param row pos of the cell
- * @param col pos of the cell
- */
-// inline Life::State Life::simulateSingleCell(size_t row, size_t col) const
-// {
-//     uint neighbors = countNeighbors(row, col);
-
-//     if (this->getCell(row, col) == State::Alive) // currently alive
-//     {
-//         if (neighbors < 2 || neighbors > 3) {
-//             return State::Dead; // Cell dies
-//         }
-//         else {
-//             return State::Alive; // Continues to live
-//         }
-//     }
-//     else // currently dead
-//     {
-//         if (neighbors == 3) {
-//             return State::Alive; // Cell becomes alive
-//         }
-//         else {
-//             return State::Dead; // Remains dead
-//         }
-//     }
-// }
-
-/**
- * Find the number of live cells around a point x,y on the grid
- *
- * @param grid world to check
- * @param row pos of the cell
- * @param col pos of the cell
- * @return number of neighbors
- */
-// inline int Life::countNeighbors(size_t row, size_t col) const
-// {
-//     int live_count = 0;
-
-//     const long row_s = static_cast<long>(row);
-//     const long col_s = static_cast<long>(col);
-
-//     // Compile-time casts required for Enum -> int conversion
-//     live_count += static_cast<int>(this->getCellWrap(row_s + 1, col_s - 1)); // top-left
-//     live_count += static_cast<int>(this->getCellWrap(row_s + 1, col_s));     // top
-//     live_count += static_cast<int>(this->getCellWrap(row_s + 1, col_s + 1)); // top-right
-
-//     live_count += static_cast<int>(this->getCellWrap(row_s, col_s - 1)); // left
-//     live_count += static_cast<int>(this->getCellWrap(row_s, col_s + 1)); // right
-
-//     live_count += static_cast<int>(this->getCellWrap(row_s - 1, col_s - 1)); // bottom-left
-//     live_count += static_cast<int>(this->getCellWrap(row_s - 1, col_s));     // bottom
-//     live_count += static_cast<int>(this->getCellWrap(row_s - 1, col_s + 1)); // bottom right
-
-//     return live_count;
-// }
-
-/**
- * Convert row,col specifier to the 1D vector access
- * Reads from current state
- */
-inline Life::State Life::getCell(size_t row, size_t col) const
-{
-    return m_bfr_current[row * m_width + col];
-}
-
-// /**
-//  * Convert row,col specifier to the 1D vector access
-//  * Reads from current state. Allows negative or overflow values to wrap around.
-//  * This has been optimized specifically for the countNeighbors operation,
-//  * after checking performance counting.
-//  */
-// inline Life::State Life::getCellWrap(long row, long col) const
-// {
-//     // row = (row + m_height) % m_height;
-//     // col = (col + m_width) % m_width;
-//     // branches were faster than modulo arithmetic
-//     // guessing branch prediction does wonders here
-//     if (row < 0) [[unlikely]] {
-//         row = m_height;
-//     };
-//     if (col < 0) [[unlikely]] {
-//         col = m_width;
-//     };
-//     if (row > static_cast<long>(m_height) - 1) [[unlikely]] {
-//         row = 0;
-//     };
-//     if (col > static_cast<long>(m_width) - 1) [[unlikely]] {
-//         col = 0;
-//     };
-//     return m_bfr_current[row * m_width + col];
-// }
-
-// /**
-//  * Convert row,col specifier to the 1D vector access.
-//  * Changes contents of next buffer
-//  * */
-// inline void Life::setCell(size_t row, size_t col, State state)
-// {
-//     m_bfr_next[row * m_width + col] = state;
-// }
